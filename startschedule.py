d86821a6fe2f5775f625b580b4fafd0ae176f51e
thr <tanghongri@123.com>
2018-05-27 21:40:53 +0800

    修改配置

--- startschedule.py
+++ startschedule.py
@@ -1,14 +1,34 @@
 import os
+import sys
 import logging
 from myschedule.model import configlogger
 from myschedule.entity import autostart
 
+# 全局参数
+g_SysConfig = {}
+
+
+def GenerateSystemConfig(sysconfig):
+    os.path.basename(sys.argv[0])
+    #程序所在目录
+    sysconfig['CUR_DIR'] = os.path.dirname(os.path.abspath(__file__))
+    #日志目录
+    sysconfig['LOG_DIR'] = os.path.join(sysconfig['CUR_DIR'], 'logs')
+    #数据存放目录
+    sysconfig['DATA_DIR'] = os.path.join(sysconfig['CUR_DIR'], 'data')
+    #临时文件目录
+    sysconfig['TEMP_DIR'] = os.path.join(sysconfig['CUR_DIR'], 'temp')
+    #检查路径存在
+    for value in sysconfig.values():
+        if not os.path.exists(value):
+            os.makedirs(value)
+
+
 if __name__ == '__main__':
-    curdir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(curdir)
+    # 获取路径信息
+    GenerateSystemConfig(g_SysConfig)
     # 配置日志
-    logdir = os.path.join(curdir, 'logs')
-    configlogger.ConfigLogger(logdir)
+    configlogger.ConfigLogger(g_SysConfig['LOG_DIR'])
 
     logger = logging.getLogger(__file__)
     logger.info("start schedule")
