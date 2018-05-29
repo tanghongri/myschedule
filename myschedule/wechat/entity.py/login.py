62c2fcd051145095eec9b5092bcddb8aa9694f92
thr <tanghongri@123.com>
2018-05-29 23:14:10 +0800

    自定义登陆qr码保存

--- myschedule/wechat/entity.py/login.py
+++ myschedule/wechat/entity.py/login.py
@@ -1,21 +1,54 @@
 import itchat
+import json
+import os
 from itchat.content import *
 '''
 微信登陆
 '''
-
-
 newInstance = itchat.new_instance()
-newInstance.auto_login(hotReload=True, statusStorageDir='newInstance.pkl')
+# qr码保持位置
+picDir = ''
 
 
 @newInstance.msg_register(TEXT)
 def reply(msg):
+    print(str(msg.text))
     return msg.text
 
 
-newInstance.run()
+def qrCallback(uuid, status, qrcode):
+    global picDir
+    if picDir:
+        with open(picDir, 'wb') as f:
+            f.write(qrcode)
 
 
-def WechatLogin():
-    pass
+def WechatLogin(sysconfig, name):
+    if 'wechat_statusStorageDir' in sysconfig:
+        statusStorageDir = sysconfig['wechat_statusStorageDir']
+        if not os.path.exists(statusStorageDir):
+            os.makedirs(statusStorageDir)
+        # statusStorageDir实际为文件
+        statusStorageDir = statusStorageDir + '/' + name + '.pkl'
+    global picDir
+    if 'wechat_picDir' in sysconfig:
+        picDir = sysconfig['wechat_picDir']
+        if not os.path.exists(picDir):
+            os.makedirs(picDir)
+        picDir = picDir + '/' + name + '.png'
+    newInstance.auto_login(
+        hotReload=True, statusStorageDir=statusStorageDir, qrCallback=qrCallback)
+    newInstance.run()
+
+
+if __name__ == '__main__':
+    sysconfig = {'wechat_picDir': r'F:\myschedule\temp\wechat\onedrive',
+                 'wechat_statusStorageDir': r'F:\myschedule\temp\wechat\status'}
+    with open(r'F:\myschedule\task.json', 'r') as jsonfile:
+        taskjosn = json.load(jsonfile)
+        for task in taskjosn:
+            if task['model'] != 'wechat':
+                continue
+            for operation in task['operations']:
+                if operation['action'] == 'login':
+                    WechatLogin(sysconfig, task['taskname'])
--- test.py
+++ test.py
@@ -3,5 +3,23 @@
 '''
 
 
-if __name__ == '__main__':
-    papa
+class testclass(object):
+    def __init__(self):
+        pass
+
+    def msg_register(self, msgType,
+                     isFriendChat=False, isGroupChat=False, isMpChat=False):
+        print("before ...")
+        print("after ...")
+
+        def _msg_register(fn):
+            print("_msg_register")
+            return fn
+        return _msg_register  
+
+
+aaa = testclass()
+
+@aaa.msg_register('123')
+def func(msg):
+    print("func was called")
